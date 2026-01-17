4e4899c05bcf5a4f82c678f2602c43d7ce19b08e
zehramans <zehramansirova0@gmail.com>
2026-01-17 13:32:17 -0500

    changes saved

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# 1. Take the input argument
 input=$1
 
+# 2. Remove the "{xor}" prefix if it exists
 clean_input="${input#"{xor}"}"
 
-base64 -d $clean_input
+# 3. Pipe the clean string into base64
+echo "$clean_input" | base64 -d
