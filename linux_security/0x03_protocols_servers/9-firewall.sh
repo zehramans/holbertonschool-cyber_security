7689dc3edaf1fa031f1bc3d89c0dc31fd54917d7
zehramans <zehramansirova0@gmail.com>
2026-02-12 06:50:24 -0500

    changes saved

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT && iptables -A INPUT -j DROP
