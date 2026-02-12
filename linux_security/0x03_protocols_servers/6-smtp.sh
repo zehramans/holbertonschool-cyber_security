9ec73da22f68c848bcd301fc29c79d449386fc83
zehramans <zehramansirova0@gmail.com>
2026-02-12 06:30:15 -0500

    changes saved

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
-#!/biin/bash
+#!/bin/bash
 awk -F'=' '/^smtpd_tls_security_level/ {gsub(/ /, "", $2); val=$2} END {print (val == "" || val == "none") ? "STARTTLS not configured" : "smtpd_tls_security_level =" val}' /etc/postfix/main.cf
