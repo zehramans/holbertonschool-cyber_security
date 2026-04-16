24212f14709e512ddb7fc3991b13aa1134f7f484
zehramans <zehramansirova0@gmail.com>
2026-04-16 08:14:39 -0400

    changes saved

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-logfile="logs.txt"
+logfile="auth.log"
 
 # Extract service names inside parentheses like (sshd:auth)
 grep -oP '\(\K[^)]+' "$logfile" | cut -d: -f1 | sort | uniq -c | sort -nr
